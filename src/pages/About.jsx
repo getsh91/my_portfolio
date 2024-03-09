019c341595968237118999a370f6c9645e274da5
getsh91 <getsheshetu19@gmail.com>
2024-03-08 18:07:35 -0800

    about page change

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,22 +11,9 @@ import "react-vertical-timeline-component/style.min.css";
 const About = () => {
   return (
     <section className='max-container'>
-      <h1 className='head-text'>
-        Hello, I'm{" "}
-        <span className='blue-gradient_text font-semibold drop-shadow'>
-          {" "}
-          Esrael
-        </span>{" "}
-        👋
-      </h1>
+     
 
-      <div className='mt-5 flex flex-col gap-3 text-slate-500'>
-        <p>
-          Software Engineer with 3+ years of experience in building web and mobile apps.
-        </p>
-      </div>
-
-      <div className='py-10 flex flex-col'>
+      <div className='py-8 flex flex-col'>
         <h3 className='subhead-text'>My Skills</h3>
 
         <div className='mt-16 flex flex-wrap gap-12'>
@@ -104,10 +91,6 @@ const About = () => {
           </VerticalTimeline>
         </div>
       </div>
-
-      <hr className='border-slate-200' />
-
-      <CTA />
     </section>
   );
 };
