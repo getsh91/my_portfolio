4129f2231567f95179c4e218e4e1c678e54bc61a
getsh91 <getsheshetu19@gmail.com>
2024-03-21 01:29:29 -0700

    final home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,13 +9,14 @@ const Home = () => {
     <div>
       <section className='relative flex lg:flex-row flex-col max-home home_back'>
         <div className="flex-1 min-w-[50%] flex flex-col">
-        <h2 className='head-text pt-5'>
-          Hello, I'm{" "}
+      
+        <h2 className='head-text2 pt-5'>
+          Hello 👋,{" "}
         </h2>
         <h1>
-        <span className='blue-gradient_text font-bold text-[6rem] max-sm:text-5xl drop-shadow'>
+        <span className=' font-bold text-[6rem] max-sm:text-5xl drop-shadow text-white'>
             {" "}
-            Esrael Eshetu
+            I'm <span className="text-[#47b84a]">Esrael Eshetu</span>
           </span>
         </h1>
         <div className='mt-5 flex flex-col gap-3 text-2xl max-sm:text-xl text-white'>
