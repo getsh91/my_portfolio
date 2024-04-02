e29cd4f35dffbc65786a08637cc7b8ba2acbc216
getsh91 <getsheshetu19@gmail.com>
2024-04-02 16:17:07 -0700

    update

--- src/components/index.js
+++ src/components/index.js
@@ -3,13 +3,11 @@ import Alert from "./Alert";
 import Loader from "./Loader";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import HomeInfo from "./HomeInfo";
 
 export {
     CTA,
     Alert,
     Loader,
     Navbar,
-    Footer,
-    HomeInfo
+    Footer
 }
